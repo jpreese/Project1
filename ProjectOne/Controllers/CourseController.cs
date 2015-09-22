db7d6fdb53d8197dc0bb7ad9e796bae433e23581
John Reese <jpreese@mtu.edu>
2015-09-21 20:44:15 -0400

    Add course view

---
+++ ProjectOne/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectOne.Controllers
+{
+    public class CourseController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
